f11cee7736a61c554031f0ab1b6d421e3c31c3ea
leaf <leaf@leafytea.host>
2024-09-25 12:07:08 -0400

    ShadCN Build

--- src/paths/Auth.jsx
+++ src/paths/Auth.jsx
@@ -74,7 +74,7 @@ function Auth() {
               className="w-full bg-white text-gray-700 p-2 rounded-lg font-semibold hover:bg-gray-100 transition duration-200 border border-gray-300 flex items-center justify-center"
             >
               <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google logo" className="w-5 h-5 mr-2" />
-              {isLogin ? 'Login with Google' : 'Register with Google'}
+              {isLogin ? 'Sign in with Google' : 'Register with Google'}
             </button>
           </div>
         </CardContent>
